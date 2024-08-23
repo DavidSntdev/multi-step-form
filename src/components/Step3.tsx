ab2ce05e2549601dbdd9e5fb5c8081107dd12e38
David Santos <162488821+DavidSntdev@users.noreply.github.com>
2024-08-23 02:15:53 -0300

    Create Step3&4

---
+++ src/components/Step3.tsx
@@ -0,0 +1,12 @@
+import { h1Class, pClass } from "../data/styles";
+
+function Step3() {
+  return (
+    <div>
+      <h1 className={h1Class}>Pick add-ons</h1>
+      <p className={pClass}>Add-ons help enhance your gaming experience</p>
+    </div>
+  );
+}
+
+export default Step3;
---
+++ src/components/Step4.tsx
@@ -0,0 +1,10 @@
+function Step4() {
+    return (
+      <div>
+        <h1>Pick add-ons</h1>
+        <p>Add-ons help enhance your gaming experience</p>
+      </div>
+    );
+  }
+  
+  export default Step4;
